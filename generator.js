03621be5f4f87ac32781bd72eee7c8829f280988
Arsham Mag.X <44603372+ArshamKhodajoo@users.noreply.github.com>
2019-08-07 19:10:10 +0430

    Update generator.js

--- generator.js
+++ generator.js
@@ -5,7 +5,7 @@ const token = "PAST_YOUR_TOKEN_RIGHT_HERE";
 const bot = new telegram_api(token , {
 
     // for some reason, Telegram forbidden in Iran, Russia and ...
-    // here we is a sample to how use SOCK% proxy (like tor) to bypass this stuff
+    // herث is a sample to how use SOCK% proxy (like tor) to bypass this stuff
 
     polling: true,
     /* request: {
