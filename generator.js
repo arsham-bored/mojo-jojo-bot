365ca2234b07cc6c0788cb777172ee5d5d483701
Arsham Mag.X <44603372+ArshamKhodajoo@users.noreply.github.com>
2019-08-07 18:59:58 +0430

    Add files via upload

--- generator.js
+++ generator.js
@@ -1,8 +1,12 @@
-const Agent = require('socks5-https-client/lib/Agent')
+//const Agent = require('socks5-https-client/lib/Agent')
 const telegram_api = require('node-telegram-bot-api')
 
 const token = "956250090:AAG3op0mNVRhY5OKQ7DJARvmafALmmq3N9w";
-const bot = new telegram_api(token, {
+const bot = new telegram_api(token /*, {
+
+    // for some reason, Telegram forbidden in Iran, Russia and ...
+    // here we is a sample to how use SOCK% proxy (like tor) to bypass this stuff
+
     polling: true,
     request: {
         agentClass: Agent,
@@ -13,8 +17,8 @@ const bot = new telegram_api(token, {
              socksUsername: '',
              socksPassword: ''
         }
-    }
-});
+    } 
+}*/ );
 
 
 module.exports = bot
